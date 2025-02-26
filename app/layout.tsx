58c97ed1d719f05cc63cd42a0db69be313024bda
Bianca <164269876+biancaalvess@users.noreply.github.com>
2025-02-26 19:50:23 -0300

    att tittle

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: 'Mouse Pro',
   description: 'Created with v0',
   generator: 'v0.dev',
 }
